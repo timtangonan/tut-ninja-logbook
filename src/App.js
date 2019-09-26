13d5cea304894802f1cea13be3366fa83e5dcad1
Tim Tangonan <trstangonan@gmail.com>
2019-09-26 10:22:09 +0800

    tutorial end

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Ninjas from './Ninjas'
+import AddNinja from './AddNinja'
 
-const App = () => {
-  return (
-    <div className="App">
-      <h1>My first React app!</h1>
-      <p>Welcome :)</p>
-    </div>
-  );
+class App extends Component {
+  state = {
+    ninjas : [
+      { name: 'Ryu', age: 30, belt: 'black', id: 1 },
+      { name: 'Yoshi', age: 20, belt: 'green', id: 2 },
+      { name: 'Crystal', age: 25, belt: 'pink', id: 3 }
+    ]
+  }
+  addNinja = (ninja) => {
+    ninja.id = Math.random();
+    let ninjas = [...this.state.ninjas, ninja]
+    this.setState({ 
+      ninjas: ninjas
+    })
+  }
+  deleteNinja = (id) => {
+    let ninjas = this.state.ninjas.filter(ninja => {
+      return ninja.id !== id
+    });
+    this.setState({
+      ninjas: ninjas
+    })
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>My first Reat app!</h1>
+        <p>Welcome :)</p>
+        <Ninjas deleteNinja={this.deleteNinja} ninjas={this.state.ninjas} />
+        <AddNinja addNinja={this.addNinja} />
+      </div>
+    )
+  }
 }
 
 export default App;
